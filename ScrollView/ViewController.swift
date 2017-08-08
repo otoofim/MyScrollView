06f3dfbf5d92ff48d0b5579a88a453fbf7a7e716
Mohammad Otoofi <Mohammad@usmacs-mbp.home>
2017-08-08 20:52:46 +0100

    swipe gesture is added to handle scrolling action in both side of scrollView

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -12,11 +12,28 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var scrollView: UIScrollView!
+
     var myuiimages = [UIImageView]()
     var contentwidth: CGFloat = 0.0
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeRight))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeLeft))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        let scrollviewWidth = scrollView.frame.size.width
+        var contentwidth: CGFloat = 0.0
         for x in 0...2
         {
             let imagee = UIImage(named: "icon\(x).png")
@@ -25,16 +42,39 @@ class ViewController: UIViewController {
             myuiimages.append(uiimageview)
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollviewWidth / 2 + scrollviewWidth * CGFloat(x)
             contentwidth += newX
-
+            
             
             scrollView.addSubview(uiimageview)
-            uiimageview.frame = CGRect.init(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            uiimageview.frame = CGRect.init(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
             
         }
         
         scrollView.contentSize = CGSize.init(width: contentwidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
+    }
+    
+    func respondToSwipeRight(gesture: UIGestureRecognizer)
+    {
+        let scrollviewWidth = scrollView.frame.size.width
+
+        var frame: CGRect = self.scrollView.frame
+        frame.origin.x = scrollView.contentOffset.x + scrollviewWidth * CGFloat(1)
+        frame.origin.y = (view.frame.size.height / 2) - 75
+        self.scrollView.scrollRectToVisible(frame, animated: true)
+    }
+    
+    
+    func respondToSwipeLeft(gesture: UIGestureRecognizer)
+    {
+        let scrollviewWidth = scrollView.frame.size.width
+        
+        var frame: CGRect = self.scrollView.frame
+        frame.origin.x = scrollView.contentOffset.x - scrollviewWidth * CGFloat(1)
+        frame.origin.y = (view.frame.size.height / 2) - 75
+        self.scrollView.scrollRectToVisible(frame, animated: true)
+
     }
 
    
